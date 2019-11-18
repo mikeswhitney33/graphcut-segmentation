2b8b81f5be8238cf22d84a0cd78503a8dfd78e00
Mike Whitney <mikeswhitney33@gmail.com>
2019-11-18 16:22:22 -0700

    fixed readme

--- graphcut.py
+++ graphcut.py
@@ -1,10 +1,16 @@
-exec env LD_LIBRARY_PATH=. python -x "$0" "$@"
 import maxflow
 import numpy as np
 from pyfigtree import figtree
 
 
+def n_weight(d, sig):
+    sig2 = 2 * sig * sig
+    return np.exp(-np.square(np.sum(d, axis=2))/sig2)
+
 def graphcut(image, fore, back, lam, sig):
-    h, w, c = image.shape
+    h, w, _ = image.shape
     graph = maxflow.GraphFloat(h * w)
     nodes = graph.add_grid_nodes((h, w))
+
+    dx = image[:,1:] - image[:,:-1]
+    dy = image[1:] - image[:-1]
